c72539758ad2ca2744fd5e3aaa04862b8ed90c77
rezkimm <rizki.snutz@gmail.com>
2016-11-29 14:20:34 +0700

    Add files via upload

---
+++ Ohm_meter.ino
@@ -0,0 +1,19 @@
+float Vin= 5,Vout,R1= 100,R2;
+void setup()
+{
+Serial.begin(9600);
+}
+void loop()
+{
+Vout= (analogRead(A0)* Vin)/1023.0;
+R2= Vout/((Vin-Vout)/R1);
+if(R2==Vout/0){
+  R2=0;
+}
+Serial.print("Vout: ");
+Serial.println(Vout);
+Serial.print("R2: ");
+Serial.println(R2);
+delay(1000);
+}
+
